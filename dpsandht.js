c9e2caa642a6ba54dd3135953c4b70af7ae0da0d
buison <buison@users.noreply.github.com>
2017-06-27 01:05:55 +0700

    Add files via upload

---
+++ dpsandht.js
@@ -0,0 +1,5 @@
+function Audio(e){amthanh=new Array,
+amthanh[1]="Đang Phát Sóng",
+amthanh[2]="Hoàn Tất",
+"Đang Phát Sóng"==e&&document.write(amthanh[1]),
+"Hoàn Tất"==e&&document.write(amthanh[2])}
