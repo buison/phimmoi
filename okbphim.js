ccba451406713223793d74dbb847755179fc6c62
buison <buison@users.noreply.github.com>
2017-06-29 17:43:29 +0700

    Add files via upload

---
+++ okbphim.js
@@ -0,0 +1,6 @@
+function titlecp(a,b,f,g,d,c,e){2E3<Number(b)&&(b=2E3);a=a.replace(/s72-c/gi,"s"+b+"");data=''+dcp(c)+"";document.write(data)};
+function tenchinh(a,b,f,g,d,c,e){2E3<Number(b)&&(b=2E3);a=a.replace(/s72-c/gi,"s"+b+"");data=''+dc(c)+"";document.write(data)};
+function tenphu(a,b,f,g,d,c,e){2E3<Number(b)&&(b=2E3);a=a.replace(/s72-c/gi,"s"+b+"");data=''+dp(c)+"";document.write(data)};
+function namphathanh(a,b,f,g,d,c,e){2E3<Number(b)&&(b=2E3);a=a.replace(/s72-c/gi,"s"+b+"");data=''+dn(c)+"";document.write(data)};
+function chatluong(a,b,f,g,d,c,e){2E3<Number(b)&&(b=2E3);a=a.replace(/s72-c/gi,"s"+b+"");data=''+dtt(c)+"";document.write(data)};
+function dophangiai(a,b,f,g,d,c,e){2E3<Number(b)&&(b=2E3);a=a.replace(/s72-c/gi,"s"+b+"");data=''+dpg(c)+"";document.write(data)};
